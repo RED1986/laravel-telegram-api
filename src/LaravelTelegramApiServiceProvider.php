c4678f61fb09102d03681eede5b5ab1dac39e0fd
Oleg Nikiforov <o.nikiforov@togas.com>
2022-07-13 15:02:33 +0300

    Add service provider

---
+++ src/LaravelTelegramApiServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * @class LaravelTelegramApiServiceProvider
+ */
+class LaravelTelegramApiServiceProvider extends ServiceProvider
+{
+    /**
+     * @inheritdoc
+     */
+    public function boot()
+    {
+        $modules = config("module.modules");
+        if ($modules) {
+            while (list(, $module) = each($modules)) {
+                if (file_exists(__DIR__ . '/' . $module . '/Routes/routes.php')) {
+                    $this->loadRoutesFrom(__DIR__ . '/' . $module . '/Routes/routes.php');
+                }
+                if (is_dir(__DIR__ . '/' . $module . '/Views')) {
+                    $this->loadViewsFrom(__DIR__ . '/' . $module . '/Views', $module);
+                }
+                if (is_dir(__DIR__ . '/' . $module . '/Migration')) {
+                    $this->loadMigrationsFrom(__DIR__ . '/' . $module . '/Migration');
+                }
+                if (is_dir(__DIR__ . '/' . $module . '/Lang')) {
+                    $this->loadTranslationsFrom(__DIR__ . '/' . $module . '/Lang', $module);
+                }
+            }
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+
+    }
+}
